6fc2ef83d3596988ed95b2d116e0dc1ed9359493
Dhanush2451 <164873180+Dhanush2451@users.noreply.github.com>
2025-05-05 10:46:29 +0530

    Create Navi.java

---
+++ Navi.java
@@ -0,0 +1,134 @@
+package com.example.navigation123;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+public class MainActivity extends AppCompatActivity {
+ Button b1,b2;
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ EdgeToEdge.enable(this);
+ setContentView(R.layout.activity_main);
+ 
+ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main
+), (v, insets) -> {
+ Insets systemBars = 
+insets.getInsets(WindowInsetsCompat.Type.systemBars());
+ v.setPadding(systemBars.left, systemBars.top, 
+systemBars.right, systemBars.bottom);
+ return insets;
+ });
+ b1 = (Button) findViewById(R.id.button);
+ b2 = (Button) findViewById(R.id.button2);
+ b1.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View view) {
+ Intent i = new Intent(getBaseContext(), Secondpage.class);
+ startActivity(i);
+ }
+ });
+ b2.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View view) {
+ Intent i = new Intent(getBaseContext(), Thirdpage.class);
+ startActivity(i);
+ }
+ });
+ }
+}
+Secondpage.java
+package com.example.navigation123;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+public class Secondpage extends AppCompatActivity {
+Button b1,b2;
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ EdgeToEdge.enable(this);
+ setContentView(R.layout.activity_secondpage);
+ 
+ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main
+), (v, insets) -> {
+ Insets systemBars = 
+insets.getInsets(WindowInsetsCompat.Type.systemBars());
+ v.setPadding(systemBars.left, systemBars.top, 
+systemBars.right, systemBars.bottom);
+ return insets;
+ });
+ b1 = (Button) findViewById(R.id.button);
+ b2 = (Button) findViewById(R.id.button2);
+ b1.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View view) {
+ Intent i = new Intent(getBaseContext(),MainActivity.class);
+ startActivity(i);
+ }
+ });
+ b2.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View view)Intent i = new 
+Intent(getBaseContext(), Thirdpage.class);
+ startActivity(i);
+ }
+ });
+ }
+}
+Thirdpage.java
+package com.example.navigation123;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+public class Thirdpage extends AppCompatActivity {
+ Button b1,b2;
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ EdgeToEdge.enable(this);
+ setContentView(R.layout.activity_thirdpage);
+ 
+ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main
+), (v, insets) -> {
+ Insets systemBars = 
+insets.getInsets(WindowInsetsCompat.Type.systemBars());
+ v.setPadding(systemBars.left, systemBars.top, 
+systemBars.right, systemBars.bottom);
+ return insets;
+ });
+ b1 = (Button) findViewById(R.id.button);
+ b2 = (Button) findViewById(R.id.button2);
+ b1.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View view) {
+ Intent i = new Intent(getBaseContext(), MainActivity.class);
+ startActivity(i);
+ }
+ });
+ b2.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View view) {
+ Intent i = new Intent(getBaseContext(), Secondpage.class);
+ startActivity(i);
+ }
+ });
+ }
+}
